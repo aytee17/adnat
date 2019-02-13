66ccdfc99587f7d856652797691c0832e6e2f4a5
Andrew Tang <andrew.tang@outlook.com>
2019-02-13 17:41:27 +1000

    Add input error message display, which takes precedence over input hints

--- src/Components/Controls/Input.js
+++ src/Components/Controls/Input.js
@@ -2,18 +2,37 @@ import React from "react";
 import style from "./Input.scss";
 import classnames from "classnames";
 
-function Input({ label, name, icon, hint, errorMessage, children, ...props }) {
+function Input({
+    label,
+    name,
+    icon,
+    hint,
+    invalid,
+    errorMessage,
+    children,
+    ...props
+}) {
     const className = classnames(style["input"], {
         [style["with-icon"]]: icon
     });
+
+    const messageClassName = classnames([style["hint"]], {
+        [style["error"]]: errorMessage
+    });
+
     return (
         <div className={style["container"]}>
-            <input className={className} placeholder={label} {...props} />
+            <input
+                className={className}
+                placeholder={label}
+                name={name}
+                {...props}
+            />
             <div className={style["label"]}>
                 <label htmlFor={label}>{label}</label>
             </div>
             {children}
-            <div className={style["hint"]}>{hint}</div>
+            <div className={messageClassName}>{errorMessage || hint}</div>
         </div>
     );
 }
