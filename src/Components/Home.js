489176eab95fe4a9388a7fec663ed0e5dfeeb9ac
Andrew Tang <andrew.tang@outlook.com>
2019-02-13 15:59:30 +1000

    Add signup to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,23 @@
 import React from "react";
 import style from "./Home.scss";
 import classnames from "classnames";
-import Login from "./Login";
+
 import Logo from "./Logo";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 function Home() {
     return (
         <>
-            <div className={style["background"]} />
-            <div className={style["home"]}>
+            <div className={style["container"]}>
                 <div className={style["top"]}>
                     <Logo />
                     <Login />
                 </div>
+            </div>
+
+            <div className={style["bottom"]}>
+                <SignUp />
             </div>
         </>
     );
