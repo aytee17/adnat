12e838b20ecaaf937687444748309071c683d3f2
Andrew Tang <andrew.tang@outlook.com>
2019-02-15 09:49:04 +1000

    Add options to logo rendering

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,7 +1,13 @@
 import React from "react";
+import style from "./Logo.scss";
+import classnames from "classnames";
 
-const Logo = () => (
-    <div style={{ fontFamily: "Patua One", fontSize: "45px" }}>Adnat</div>
-);
+const Logo = ({ small, white }) => {
+    const className = classnames(style["logo"], {
+        [style["white"]]: white,
+        [style["small"]]: small
+    });
+    return <div className={className}>Adnat</div>;
+};
 
 export default Logo;
