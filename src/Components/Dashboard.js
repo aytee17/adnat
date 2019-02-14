7a33e1e365682f963b9ad1a5af3d732a5f8366c9
Andrew Tang <andrew.tang@outlook.com>
2019-02-14 18:48:41 +1000

    Add dashboard

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import style from "./Dashboard.scss";
+import { api } from "../utils/api";
+
+function Dashboard() {
+    return "hello";
+}
+
+export default Dashboard;
