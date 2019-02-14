68a506f4a0ec7b7318cd208794ab9350fa92199d
Andrew Tang <andrew.tang@outlook.com>
2019-02-14 18:46:15 +1000

    Add account and down icons

--- src/Components/Icons/Icons.js
+++ src/Components/Icons/Icons.js
@@ -1,5 +1,6 @@
 import React from "react";
 import style from "./Icons.scss";
+import classnames from "classnames";
 
 export const MailIcon = () => (
     <svg
@@ -72,3 +73,36 @@ export const TickIcon = () => (
         <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
     </svg>
 );
+
+export const AccountIcon = () => (
+    <svg
+        className={style["button-icon"]}
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+    >
+        <path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2zm12 4c0 1.66-1.34 3-3 3s-3-1.34-3-3 1.34-3 3-3 3 1.34 3 3zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6v-1z" />
+        <path d="M0 0h24v24H0z" fill="none" />
+    </svg>
+);
+
+export const DownIcon = ({ open }) => {
+    const classNames = classnames(style["down-icon"], {
+        [style["open"]]: open
+    });
+
+    return (
+        <svg
+            className={classNames}
+            xmlns="http://www.w3.org/2000/svg"
+            x="0"
+            y="0"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+        >
+            <path d="M7.41,8.59L12,13.17l4.59-4.58L18,10l-6,6l-6-6L7.41,8.59z" />
+        </svg>
+    );
+};
