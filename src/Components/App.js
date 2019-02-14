30628fb805766eaf378448bff4c09487eb693cda
Andrew Tang <andrew.tang@outlook.com>
2019-02-14 23:01:06 +1000

    Securely(?) persist login state and auth token across browsing contexts

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,32 @@
 import React, { useState, useEffect } from "react";
+import SecureLS from "secure-ls";
 import { Router } from "@reach/router";
+import { hot } from "react-hot-loader";
+import { api, setAuth } from "../utils/api";
+
 import Home from "./Home";
 import Dashboard from "./Dashboard";
-import { hot } from "react-hot-loader";
-import { setAuth } from "../utils/api";
 import NavBar from "./NavBar";
-import { api } from "../utils/api";
+
+const storage = new SecureLS({ encodingType: "aes" });
+const channel = new BroadcastChannel("session");
 
 function App() {
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [user, setUser] = useState({ name: "", email: "" });
-    const { name, email } = user;
+    const [loggedIn, setLoggedIn] = useState(() => {
+        const alreadyIn = storage.get("loggedIn");
+        if (alreadyIn === true) {
+            const sessionID = storage.get("sessionID");
+            setAuth(sessionID);
+            return true;
+        }
+        return false;
+    });
+
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        organisationId: undefined
+    });
 
     useEffect(() => {
         if (loggedIn) {
@@ -20,16 +36,44 @@ function App() {
         }
     }, [loggedIn]);
 
-    function login(sessionID) {
+    useEffect(() => {
+        channel.onmessage = ({ data }) => {
+            const { type, payload } = data;
+            console.log(data);
+            switch (type) {
+                case "LOGIN":
+                    login(payload, false);
+                    break;
+                case "LOGOUT":
+                    logout(false);
+                    break;
+            }
+        };
+        return () => channel.close();
+    }, []);
+
+    function login(sessionID, selfInitiated) {
         setAuth(sessionID);
         setLoggedIn(true);
+        if (selfInitiated === true || selfInitiated === undefined) {
+            storage.set("sessionID", sessionID);
+            storage.set("loggedIn", true);
+            channel.postMessage({ type: "LOGIN", payload: sessionID });
+        }
     }
 
-    function logout() {
+    function logout(selfInitiated) {
         setAuth("");
         setLoggedIn(false);
+
+        if (selfInitiated === true || selfInitiated === undefined) {
+            storage.set("sessionID", "");
+            storage.set("loggedIn", false);
+            channel.postMessage({ type: "LOGOUT", payload: null });
+        }
     }
 
+    const { name, email } = user;
     return !loggedIn ? (
         <Router>
             <Home path="/" login={login} />
@@ -38,7 +82,7 @@ function App() {
         <div>
             <NavBar name={name} email={email} logout={logout} />
             <Router>
-                <Dashboard path="/" name={name} />
+                <Dashboard path="/" user={user} setUser={setUser} />
             </Router>
         </div>
     );
