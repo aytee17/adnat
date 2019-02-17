4f493d937382c9554dc4d15a6ddb0d6443e6257e
Andrew Tang <andrew.tang@outlook.com>
2019-02-17 19:44:33 +1000

    Add shift list component

---
+++ src/Components/Pages/ShiftList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./ShiftList.scss";
+
+function ShiftList({ shifts }) {
+    return (
+        <div>
+            <div>Shifts</div>
+        </div>
+    );
+}
+
+export default ShiftList;
