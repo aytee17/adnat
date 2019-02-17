224d2fba8a16308f5b7ee42da0ce4b554474c857
Andrew Tang <andrew.tang@outlook.com>
2019-02-18 00:16:50 +1000

    Add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,10 @@ module.exports = {
                 }
             },
             {
+                test: /\.css$/,
+                use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+            },
+            {
                 test: /\.scss$/,
                 use: [
                     {
